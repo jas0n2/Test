7c89a120fa583751ef1f90d772fcd2827c8b564a
ceaser <ceaserjayson@gmail.com>
2024-01-24 07:27:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.post('/api/products', (req, res) => {
 app.put('/api/products/:id', async (req, res) => {
   const productId = req.params.id;
   const updatedProduct = req.body;
-const count = await db('products').where({id}).update(updatedProduct);
+const count = await db('products').where({productId}).update(updatedProduct);
   // Check for existing product with the same name and category
   db('products').select('*').where('name', updatedProduct.name).where('cate', updatedProduct.cate).whereNot('id', productId)
     .then((results) => {
